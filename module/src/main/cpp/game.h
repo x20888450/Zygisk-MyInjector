cb8cd0657e8146f1a1fc9b6b355a062a6c7270cf
开源群的樱 <131824670+x20888450@users.noreply.github.com>
2024-11-28 21:17:28 +0800

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define AimPackageName "com.tencent.mobileqq"
+#define AimPackageName "com.tencent.af"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
